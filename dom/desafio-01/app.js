e5c06a6c0af0f071b27937be60d24b0499d92ab2
Jean Pereira <jeancarlo.npereira@gmail.com>
2020-09-09 20:52:00 -0300

    criando evento com v-on

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,16 @@
+
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Jean',
+        idade: 26,
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ53Ya7OiOk9ghCpsv3XYgzoXnn8SdihfZSkg&usqp=CAU'
+    },
+    methods:{
+        randNumber(){
+            return Math.random()
+        }
+        
+    }
+
+    })
